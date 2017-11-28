355c952488ef2b36dcc51376a48bd13a045f055f
Kenneth So <ethnographers@users.noreply.github.com>
2017-11-27 21:26:48 -0800

    unity

---
+++ RasaUnity/Assets/Network.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using SocketIO;
+
+
+public class Network : MonoBehaviour {
+
+	static SocketIOComponent socket;
+
+	void Start ()
+	{
+		socket = GetComponent<SocketIOComponent> ();
+		socket.On ("open", OnConnected);
+		socket.On ("data", OnData);
+		
+	}
+	
+	void OnConnected(SocketIOEvent e)
+	{
+		Debug.Log ("connected");
+		Debug.Log (e);
+		socket.Emit("test");
+	}
+
+	void OnData(SocketIOEvent obj)
+	{
+		Debug.Log (obj);
+	}
+}
